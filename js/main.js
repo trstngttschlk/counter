f805b1ca0ac0f497c946cf3da41c37d7dc6a3c10
Tristan Gottschalk <trstn@gttschlk.com>
2022-04-27 10:58:04 -0400

    add color change when neg / pos value

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ let count = 0
 let h2 = document.querySelector('h2')
 
 // assign text in h2 to count
-h2.innerText = count
+h2.textContent = count
 
 // event listeners for buttons
 decrease.addEventListener('click',decreaseCount)
@@ -19,13 +19,25 @@ increase.addEventListener('click',increaseCount)
 
 // functions
 function decreaseCount(){
-    h2.innerText--
+    h2.textContent--
+    changeColor()
 }
 
 function resetCount(){
-    h2.innerText = 0
+    h2.textContent = 0
+    h2.style.color = 'black'
 }
 
 function increaseCount(){
-    h2.innerText++
+    h2.textContent++
+    changeColor()
+}
+
+// change color based on neg / pos value
+function changeColor(){
+    if (h2.textContent < 0){
+        h2.style.color = 'red'
+    } else if (h2.textContent > 0){
+        h2.style.color = 'green'
+    }
 }
